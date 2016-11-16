e95e33bdba32d1f256048a0543278759df7953bc
ahf5 <ahf5@pitt.edu>
2016-11-15 21:08:36 -0500

    Added triggers for cancelReservation and planeUpgrade. To do: add to cancelReservation so that when a reservation is deleted it checks the number of remaining reservations to see if they can fit into a smaller plane. Also, do adjust ticket

--- Schema.sql
+++ Schema.sql
@@ -61,8 +61,7 @@ CREATE TABLE Flight (
   Weekly_Schedule varchar2(7),
   Constraint flight_PK primary key (Flight_Number) deferrable,
   Constraint flight_FK1 foreign key (Plane_Type, Airline_ID) references Plane(Plane_Type, Owner_ID) initially deferred deferrable,
-  Constraint flight_FK2 foreign key (Airline_ID) references Airline(Airline_ID) initially deferred deferrable,
-  CONSTRAINT flight_CS CHECK (Departure_Time < Arrival_Time));
+  Constraint flight_FK2 foreign key (Airline_ID) references Airline(Airline_ID) initially deferred deferrable);
   
   /* The handout says "plane_type" of this table should be a char instead of varchar, but you get an error when you reference that field from Plane that has another data type - assuming it should be matching data type*/
   
@@ -135,7 +134,7 @@ CREATE TABLE Reservation_Detail (
   Flight_Date date,
   Leg int,
   Constraint reservation_detail_PK primary key (Reservation_Number) deferrable,
-  Constraint reservation_detail_FK1 foreign key (Reservation_Number) references Reservation(Reservation_number) initially deferred deferrable,
+  Constraint reservation_detail_FK1 foreign key (Reservation_Number) references Reservation(Reservation_number) on delete cascade initially deferred deferrable,
   Constraint reservation_detail_FK2 foreign key (Flight_Number) references Flight(Flight_Number) initially deferred deferrable);
 
 
@@ -151,8 +150,6 @@ CREATE TABLE PDate (
 
 
 
-/* --- TRIGGERS --- */
-
 /* Trigger 1 - adjustTicket */
 /* Adjusts the cost of a reservation when the price of one of its legs changes before the ticket is issued */
 
@@ -167,15 +164,93 @@ CREATE TABLE PDate (
 
 
 
-
-
 /* Trigger 2 - planeUpgrade */
+
+--function for finding a bigger plane
+CREATE OR REPLACE FUNCTION findBiggerPlane(numReserved in int,planeOwner in varchar2) 
+return varchar2 is
+anyPlanes int;
+biggerPlane varchar2(4);
+begin
+	--find how many planes are bigger than the current plane
+	select count(plane_type) into anyPlanes from plane where owner_id = planeOwner and plane_capacity > numReserved;
+	if anyPlanes = 0 then 
+		return NULL; --if there aren't any more planes
+	else
+		--get the next biggest plane that is owned by the airline offering the flight
+		select plane_type into biggerPlane from plane where owner_id = planeOwner and plane_capacity = (select min(plane_capacity) from plane where owner_id = planeOwner and plane_capacity > numReserved);
+		return (biggerPlane);
+	end if;
+end;
+/
+
+--stored procedure that updates the flight with a new plane
+CREATE OR REPLACE PROCEDURE updatePlane(flightNum in varchar2, planeType in varchar2)
+AS
+BEGIN
+	update Flight set Plane_Type = planeType where Flight_Number = flightNum;
+END;
+/
+
 /* changes the plane (type) of a flight to an immediately higher capacity plane (type), if it exists, 
 when a new reservation is made on that flight and there are no available seats (i.e., the flight is fully booked). 
 A change of plane will fail only if the currently assigned plane for the flight is the one with the biggest capacity. 
 For simplicity, we assume that there are always available planes for a switch to succeed. */
 
+CREATE OR REPLACE TRIGGER planeUpgrade
+BEFORE INSERT ON Reservation_Detail
+FOR EACH ROW
+DECLARE numReserved int;
+		planeCap int;
+		planeType varchar(4);
+		planeOwner varchar2(5);
+BEGIN
+	--count the number of reserved seats for the current flight
+	select count(reservation_number) into numReserved from reservation_detail where flight_number = (:new.Flight_Number);
+	numReserved := numReserved + 1; --add one because it is BEFORE insert, count does not account for the row we are adding
+	--get the capacity and owner of the plane
+	select plane_capacity, owner_id into planeCap, planeOwner from (flight natural join plane) where flight_number = :new.Flight_Number;
+	if (numReserved > planeCap) then --if there are more reservations than space on the flight
+		planeType := findBiggerPlane(numReserved,planeOwner); --see if a bigger plane can be found
+		if planeType IS NULL then
+			update reservation set Ticketed = 'N' where reservation_number = :new.reservation_number; --if there is not a bigger plane then the person does not get a ticket
+		else
+			update reservation set Ticketed = 'Y' where reservation_number = :new.reservation_number; --if there is a plane, they do get a ticket
+			updatePlane(:new.flight_number, planeType); --update the plane to the larger size
+		end if;
+	else
+		--if there is less reservations than capacity, the person gets a ticket
+		update reservation set Ticketed = 'Y' where reservation_number = :new.reservation_number;
+	end if;
+END;
+/
+
+
 /* Trigger 3 - cancelReservation */
+--stored procedure for deleting non-ticketed reservations
+--Inputs: current time
+--Description
+--Subtract the current time from the time of the flight (number of days)
+--	multiply the time by 24 to get the number of hours
+--If the flight is less than or equal to 12 hours away, select that reservation number
+--If the reservation is not ticketed and it's reservation number 
+--	is in the reservation numbers that are selected, delete it
+CREATE OR REPLACE PROCEDURE deleteReservations(currentDate in DATE)
+AS
+BEGIN
+	DELETE FROM Reservation 
+	WHERE Ticketed = 'N' AND Reservation_Number IN (select Reservation_Number from Reservation_Detail where (24*(Flight_Date - currentDate)) <= 12); 
+END;
+/
+
+--TRIGGER 3
 /* cancels(deletes)all non-ticketed reservations for a flight, 12 hours prior the flight 
 (i.e., 12 hours before the flight is scheduled to depart) and if the number of ticketed passengers fits in a smaller capacity plane, 
 then the plane for that flight should be switched to the smaller-capacity plane. */
+CREATE OR REPLACE TRIGGER cancelReservation 
+AFTER UPDATE ON PDate
+FOR EACH ROW
+BEGIN
+	deleteReservations(:new.C_Date);
+END;
+/
